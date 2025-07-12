4f01939cf3fdc8d0821f4fda1fa8eeb497331bef
omkansara <okansara8@gmail.com>
2025-07-12 10:59:37 +0530

    landing page

---
+++ components/Footer.tsx
@@ -0,0 +1,53 @@
+import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  const footerItems = [
+    {
+      name: "github",
+      icon: <GitHubLogoIcon />,
+      href: "https://github.com/shravanasati/stackit",
+    },
+    {
+      name: "instagram",
+      icon: <InstagramLogoIcon />,
+      href: "https://instagram.com/",
+    }
+  ];
+
+  return (
+    <footer className="relative min-h-[20vh] bg-[#090909] overflow-hidden border-t-2 border-dashed">
+      <div className="flex flex-col md:flex-row gap-8 px-6 sm:px-8 md:px-12 lg:px-20 py-8 justify-between items-start z-10 relative w-full max-w-7xl mx-auto">
+        <div className="text-white">
+          <h3 className="text-2xl font-black text-primary mb-4">StackIt</h3>
+          <p className="text-lg text-gray-400 max-w-xs">
+            Made with ❤️ by GoGrep
+          </p>
+        </div>
+        <div className="flex flex-col items-start">
+          <h4 className="text-lg font-semibold mb-4 text-white">
+            Connect With Us
+          </h4>
+          <div className="flex flex-wrap gap-4 relative">
+            {/* map */}
+            {footerItems.map((item, index) => {
+              return (
+                <Link
+                  key={index}
+                  href={item.href}
+                  target="_blank"
+                  className="size-10 sm:size-12 md:size-14 border border-primary/50 flex justify-center items-center rounded-lg cursor-pointer group hover:border-primary transition-colors text-primary/50 hover:text-primary"
+                >
+                  <span className="scale-[2]">{item.icon}</span>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/LandingPage/Section3.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
+import { motion } from "framer-motion";
+
+const testimonials = [
+  {
+    quote:
+      "A feed that curates content across all boards and gives you a glimpse of what's happening in the community while respecting your short lived attention span.",
+    name: "Feed",
+    src: "/parallax/feed.jpg",
+  },
+  {
+    quote:
+      "Post your thoughts, questions, or anything you want to share with the community with complete anonymity without the fear of being judged.",
+    name: "Posts",
+    src: "/parallax/create.jpg",
+  },
+  {
+    quote:
+      "Interact with posts by upvoting or downvoting them. No one will know what you upvoted or downvoted.",
+    name: "Interact",
+    src: "/parallax/updownvote.jpg",
+  },
+  {
+    quote:
+      "Comment on posts and interact with the community. No one will know who you are.",
+    name: "Comments",
+    src: "/parallax/comments.jpg",
+  },
+  {
+    quote:
+      "Get notified when someone interacts with your posts or comments.",
+    name: "Notifications",
+    src: "/parallax/notifications.jpg",
+  },
+];
+
+const titleVariants = {
+  hidden: {
+    opacity: 0,
+    y: -20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+    },
+  },
+};
+
+export default function Section3() {
+  return (
+    <div className="min-h-[80vh]">
+      <motion.h2
+        className="text-4xl md:text-5xl font-bold text-center  mt-4 -mb-12 md:mb-0 text-secondary"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, margin: "-100px" }}
+        variants={titleVariants}
+      >
+        StackIt has....
+      </motion.h2>
+      <AnimatedTestimonials testimonials={testimonials} autoplay={true} />
+    </div>
+  );
+}
