d272cd9a778635b2e97a0d301686e383d8d25371
Shravan Asati <dev.shravan@protonmail.com>
2025-07-12 12:11:12 +0530

    create tos page

---
+++ app/tos/page.tsx
@@ -0,0 +1,10 @@
+import { TOS } from "@/components/TOS";
+
+export const metadata = {
+  title: "Terms of Service",
+  description: "Terms of Service for Everynyan",
+};
+
+export default function TermsOfService() {
+  return <TOS />;
+}
